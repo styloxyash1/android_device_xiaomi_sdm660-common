7d3d02b9f21faef4a61ec49569752e634a207366
zaidkhan0997 <kzaid0997@gmail.com>
2021-04-09 01:40:54 +0530

    sdm660-common: add vendorsetup
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+export SKIP_ABI_CHECKS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
+
+# nuke
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+rm -rf hardware/qcom-caf/msm8998/audio
+
+# clone 
+git clone -b 11 https://github.com/zaidkhan0997/vender_xiaomi_GoogleCamera.git vendor/xiaomi/GoogleCamera 
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_display_msm8998.git hardware/qcom-caf/msm8998/display
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_media_msm8998.git hardware/qcom-caf/msm8998/media
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_audio_msm8998.git hardware/qcom-caf/msm8998/audio
